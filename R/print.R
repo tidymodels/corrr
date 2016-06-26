53542188f9fe0ff65cfbd17423d2575457ceb8c5
drsimonj <drsimonjackson@gmail.com>
2016-06-26 18:01:46 +1000

    Added subspace functions for rows and columns

--- R/print.R
+++ R/print.R
@@ -1,10 +1,15 @@
 #' @export
 print.r_mat <- function(x, round_to = 2) {
-  class(x) <- c("matrix")
   x <- format(round(x, round_to), nsmall = round_to)  # round decimal points
   x <- gsub("0\\.", ".", x)  # remove leading zeros
-  diag(x) <- ""  # remove 1s on diagonal
-  print(noquote(x))  # print without quotes
+
+  # remove standardised variances (1s)
+  is_sv <- sapply(colnames(x), function(i) i == rownames(x))
+  x[is_sv] <- ""
+
+  # print without quotes
+  class(x) <- c("matrix")
+  print(noquote(x))
 }
 
 #' @export
