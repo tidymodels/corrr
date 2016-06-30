f51042c94b3353e115ced6b7e3abb32e6f9b953a
drsimonj <drsimonjackson@gmail.com>
2016-06-30 18:27:14 +1000

    Minor update

--- R/output.R
+++ R/output.R
@@ -2,7 +2,7 @@
 #' 
 #' Plot a \code{\link{correlate}} correlation matrix.
 #' 
-#' @param x A \code{\link{correlate}} correlation matrix.
+#' @param x cor_df. See \code{\link{correlate}}.
 #' 
 #' @export
 rplot <- function(x) {
