b86be0ecc4776684ecc3c11cee8518eb9346b708
drsimonj <drsimonjackson@gmail.com>
2016-07-04 18:43:40 +1000

    Add rplot() examples

--- R/output.R
+++ R/output.R
@@ -1,11 +1,19 @@
 #' Plot correlation matrix.
 #' 
-#' Plot a \code{\link{correlate}} correlation matrix.
+#' Plot a correlation data frame using ggplot.
 #' 
 #' @param x cor_df. See \code{\link{correlate}}.
 #' @param shape \code{\link{geom_point}} aesthetic.
-#' 
+#' @return Plots a correlation data frame
 #' @export
+#' @examples 
+#' x <- correlate(mtcars)
+#' rplot(x)
+#' 
+#' # Common use is following rearrange and shave
+#' x <- rearrange(x, absolute = FALSE)
+#' x <- shave(x)
+#' rplot(x)
 rplot <- function(x, shape) {
   UseMethod("rplot")
 }
