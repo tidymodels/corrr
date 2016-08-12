c393f129cc8c7aae4e67ee68c31281dc7472052f
drsimonj <drsimonjackson@gmail.com>
2016-08-13 09:36:02 +1000

    Minor fix: fashion.matrix

--- R/matrix.R
+++ R/matrix.R
@@ -1,6 +1,6 @@
 #' @export
-fashion.matrix <- function(x, decimals = 2, na_print = "") {
+fashion.matrix <- function(x, decimals = 2, leading_zeros = FALSE, na_print = "") {
   x %>%
     as.data.frame() %>%
-    fashion(decimals = decimals, na_print = na_print)
+    fashion(decimals = decimals, leading_zeros = leading_zeros, na_print = na_print)
 }
