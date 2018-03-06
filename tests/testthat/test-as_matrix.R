e496d583199a5c4666a9313d1479406757eb1507
cimentadaj <cimentadaj@gmail.com>
2018-03-06 18:19:49 +0100

    Close 6

---
+++ tests/testthat/test-as_matrix.R
@@ -0,0 +1,24 @@
+context("test-as_matrix.R")
+
+x <- correlate(mtcars)
+y <- as_matrix(x)
+
+test_that("Inherits correct classes", {
+  expect_is(y, "matrix")
+})
+
+test_that("Converts values accurately", {
+  expect_true(all(x[-1] == y, na.rm = TRUE))
+})
+
+test_that("Yields correct columns and rows", {
+  expect_equal(colnames(x), c("rowname", colnames(y)))
+  expect_equal(nrow(x), ncol(y))
+  expect_equal(x$rowname, colnames(y))
+  expect_equal(rownames(y), colnames(y))
+})
+
+test_that("Diagonal sets correctly", {
+  expect_true(all(diag(y) == 1))
+  expect_true(all(diag(as_matrix(x, diagonal = 100)) == 100))
+})
