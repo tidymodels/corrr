6c6df63df0457975203209b32f295cf2334e0121
Edgar Ruiz <edgararuiz@gmail.com>
2018-10-06 20:08:09 -0500

    Custom function for tbl_spark objects

--- R/correlate.R
+++ R/correlate.R
@@ -64,82 +64,119 @@ correlate.default <- function(x, y = NULL,
   
   as_cordf(x, diagonal = diagonal)
 }
+
+spark_correlate <- function(x, method = "pearson") {
+  spark_corr <- ml_corr(x, method = method)
+  
+  local_corr <- map_df(
+    spark_corr,
+    ~{
+      ones <- .x == 1
+      res <- as.numeric(.x)
+      res[ones] <- NA
+      res
+    }
+  )
+  
+  as.tibble(cbind(
+    tibble(rowname = colnames(spark_corr)),
+    local_corr
+  ))
+}
+
 #' @export
 correlate.tbl_sql <- function(x, y = NULL,
                               use = "pairwise.complete.obs",
                               method = "pearson",
                               diagonal = NA,
                               quiet = FALSE) {
-  
-  if(method != "pearson")   stop("Only person method is currently supported")
+
   if(use != "complete.obs") stop("Only 'complete.obs' method are supported")
   if(!is.null(y))           stop("y is not supported for tables with a SQL back-end")
   if(!is.na(diagonal))      stop("Only NA's are supported for same field correlations")
-  
-  df <- x
-  col_names <- colnames(df)
-  col_no <- length(col_names)
-  
-  combos <- 1:(col_no - 1) %>%
-    map_df(~tibble(
-      x = col_names[.x],
-      y = col_names[(.x + 1):col_no],
-      cn = paste0(x, "_", y)
-    ))
-  
-  full_combos <- 1:(col_no) %>%
-    map_df(~
-             tibble(
-               x = col_names[.x],
-               y = col_names[.x:col_no],
-               cn = paste0(x, "_", y)
-             ) %>%
-             bind_rows(
-               tibble(
-                 x = col_names[.x:col_no],
-                 y = col_names[.x],
-                 cn = paste0(y, "_", x)
-               )
-             )) %>%
-    filter(x != y)
-  
-  df <- df %>%
-    filter_all(all_vars(!is.na(.))) 
-  
-  df_mean <- df %>%
-    summarise_all(mean, na.rm = TRUE)
-  
-  cor_f <- 1:nrow(combos) %>%
-    map(
-      ~ tidyeval_cor(
-        !! sym(combos$x[.x]),
-        !! sym(combos$y[.x]),
-        pull(select(df_mean, combos$x[.x])),
-        pull(select(df_mean, combos$y[.x])) 
-      )
+  df_cor <- NULL
+    
+  if("tbl_spark" %in% class(x)){
+    
+    if(!method %in% c("pearson", "spearman"))
+      stop("Only person or spearman methods are currently supported")
+    
+    df_cor <- spark_correlate(
+      x = x,
+      method = method
     )
+  }
   
-  cor_f <- set_names(cor_f, combos$cn)
-  
-  df_cor <- df %>%
-    summarise(!!! cor_f) %>%
-    collect() %>%
-    as.tibble() %>%
-    tidyr::gather(cn, cor) %>%
-    right_join(full_combos, by = "cn") %>%
-    select(-cn) %>%
-    tidyr::spread(y, cor) %>%
-    rename(rowname = x) %>% 
-    # Put cols and rows into original order
-    select(rowname, !!col_names) %>% 
-    {.[match(col_names, .$rowname),]}
-  
-  class(df_cor) <- c("cor_df", class(df_cor))
-  
-  if (!quiet)
-    message("\nCorrelation method: '", method, "'",
-            "\nMissing treated using: '", use, "'\n")
-  
+  if(is.null(df_cor)){
+    
+    if(method != "pearson")   stop("Only person method is currently supported")
+    
+    df <- x
+    col_names <- colnames(df)
+    col_no <- length(col_names)
+    
+    combos <- 1:(col_no - 1) %>%
+      map_df(~tibble(
+        x = col_names[.x],
+        y = col_names[(.x + 1):col_no],
+        cn = paste0(x, "_", y)
+      ))
+    
+    full_combos <- 1:(col_no) %>%
+      map_df(~
+               tibble(
+                 x = col_names[.x],
+                 y = col_names[.x:col_no],
+                 cn = paste0(x, "_", y)
+               ) %>%
+               bind_rows(
+                 tibble(
+                   x = col_names[.x:col_no],
+                   y = col_names[.x],
+                   cn = paste0(y, "_", x)
+                 )
+               )) %>%
+      filter(x != y)
+    
+    df <- df %>%
+      filter_all(all_vars(!is.na(.))) 
+    
+    df_mean <- df %>%
+      summarise_all(mean, na.rm = TRUE)
+    
+    cor_f <- 1:nrow(combos) %>%
+      map(
+        ~ tidyeval_cor(
+          !! sym(combos$x[.x]),
+          !! sym(combos$y[.x]),
+          pull(select(df_mean, combos$x[.x])),
+          pull(select(df_mean, combos$y[.x])) 
+        )
+      )
+    
+    cor_f <- set_names(cor_f, combos$cn)
+    
+    df_cor <- df %>%
+      summarise(!!! cor_f) %>%
+      collect() %>%
+      as.tibble() %>%
+      tidyr::gather(cn, cor) %>%
+      right_join(full_combos, by = "cn") %>%
+      select(-cn) %>%
+      tidyr::spread(y, cor) %>%
+      rename(rowname = x) %>% 
+      # Put cols and rows into original order
+      select(rowname, !!col_names) %>% 
+      {.[match(col_names, .$rowname),]}
+    
+  }
+ 
+  if(!is.null(df_cor)){
+    class(df_cor) <- c("cor_df", class(df_cor))
+    if (!quiet)
+      message("\nCorrelation method: '", method, "'",
+              "\nMissing treated using: '", use, "'\n")
+  }
   df_cor
 }
 
