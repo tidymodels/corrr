9c6c53fde1331ac80373147b3c72e4a4c294cba2
Edgar Ruiz <edgar@rstudio.com>
2019-07-09 17:35:28 -0500

    Removes pipes

--- R/matrix.R
+++ R/matrix.R
@@ -1,6 +1,9 @@
 #' @export
 fashion.matrix <- function(x, decimals = 2, leading_zeros = FALSE, na_print = "") {
-  x %>%
-    as.data.frame() %>%
-    fashion(decimals = decimals, leading_zeros = leading_zeros, na_print = na_print)
+  fashion(
+    as.data.frame(x),
+    decimals = decimals, 
+    leading_zeros = leading_zeros, 
+    na_print = na_print
+    )
 }
