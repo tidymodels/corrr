1e8e53df4b721b25337f03e5b83b3061e587337f
Michael Hahsler <mhahsler@smu.edu>
2023-07-20 18:47:33 -0500

    Better calculation of deltas (distances) based on correlations. Fixed the unused absolute parameter. All seriaiton methods can now be used. Updated man page.

--- R/internal.R
+++ R/internal.R
@@ -22,19 +22,30 @@ shave <- function(x, upper = TRUE) {
 #' together.
 #'
 #' @param x cor_df. See \code{\link{correlate}}.
-#' @param method String specifying the arrangement (clustering) method.
-#'   Clustering is achieved via \code{\link[seriation]{seriate}}, which can be
+#' @param method String specifying the arrangement method.
+#'   Reordering is achieved via \code{\link[seriation]{seriate}}, which can be
 #'   consulted for a complete list of clustering methods. Default = "PCA".
 #' @param absolute Boolean whether absolute values for the correlations should
-#'   be used for clustering.
+#'   be used for reordering.
 #' @return cor_df. See \code{\link{correlate}}.
 #' @export
+#' @references
+#' Hahsler M, Hornik K, Buchta C (2008). "Getting things in order:
+#' An introduction to the R package seriation."
+#' Journal of Statistical Software, 25(3), 1-34. ISSN 1548-7660.
+#' \doi{10.18637/jss.v025.i03}
 #' @examples
 #' x <- correlate(mtcars)
 #'
-#' rearrange(x) # Default settings
-#' rearrange(x, method = "HC") # Different seriation method
-#' rearrange(x, absolute = FALSE) # Not using absolute values for arranging
+#' rearrange(x) # Default settings (PCA)
+#' rearrange(x, method = "PCA_angle") # Angle in 2D PCA projection
+#' rearrange(x, method = "OLO") # Optimal leaf ordering
+#' rearrange(x, method = "R2E") # Rank 2 ellipse seriation
+#'
+#' rearrange(x, absolute = TRUE) # Using absolute values for arranging
+#'
+#' # list all available seriation methods
+#' seriation::list_seriation_methods()
 rearrange <- function(x, method = "PC", absolute = TRUE) {
   UseMethod("rearrange")
 }
